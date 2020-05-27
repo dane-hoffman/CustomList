6d1ec2a5073fdf8b5802e248e63c27d2f7a444c4
unknown <dane.hoffman90@gmail.com>
2020-05-27 10:02:40 -0600

    Recreated 'Testing Class', created methods for the basic functions of the CustomList 'Array'.

--- UnitTest1/UnitTest1.cs
+++ UnitTest1/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using CustomListProject;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Custom_List_Project;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CustomListUnitTest
@@ -8,10 +10,23 @@ namespace CustomListUnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        private void CreateListSize()
         {
+            CustomList customList = new CustomList();
+            double capacity = 4;
+            double expectedCount = count;
+            double actualCount;
 
+
+            actualCount = customList.array.count
+
+
+            Assert.IsActual(actualCount, expectedCount);
 
         }
+
+        [TestMethod]
+
+        private void AddToList
     }
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Custom_List_Project;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        private void Create_Array()
+        {
+            //Arrange
+            CustomList customList = new CustomList();
+
+            //Act
+            customList = new CustomList();
+
+            //Assert
+            Assert.IsNotNull(customList);
+
+        }
+
+
+        [TestMethod]
+        private void Confirm_Array_Capacity()
+        {
+            //Arrange
+            CustomList customList = new CustomList();
+            
+            double capacity = 4;
+            double expectedCapacity = capacity;
+            double actualCapacity;
+            double index = capacity;
+
+            //Act
+            actualCapacity = expectedCapacity - capacity;
+
+            //Assert
+            Assert.AreEqual(actualCapacity, capacity);
+
+        }
+
+
+        [TestMethod]
+        private void Increase_Array_Capacity()
+        {
+            //Arrange
+            CustomList customList = new CustomList();
+            double capacity = 4;
+            double increaseIndex;
+            bool indexDoesIncrease = true;
+
+            //Act
+            increaseIndex = capacity ++;
+
+
+            //Assert
+            Assert.IsTrue(indexDoesIncrease);
+        }
+
+
+
+        [TestMethod]
+        private void Decrease_Array_Capacity()
+        {
+
+            //Arrange
+            CustomList customList = new CustomList();
+            double capacity = 4;
+            double decreaseIndex;
+            bool indexDoesDecrease = true;
+
+            //Act
+            decreaseIndex = capacity--;
+
+            //Assert
+            Assert.IsTrue(indexDoesDecrease);
+
+
+        }
+
+        [TestMethod]
+        private void Confirm_Array_Count()
+        {
+
+
+        }
+
+    }
+}
